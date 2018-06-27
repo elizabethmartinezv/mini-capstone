191f3e2c39e262e09e3fdb4ec36abe4431eed91f
“Elizabeth <lizzyadeline@gmail.com>
2018-06-26 19:07:43 -0500

    Created data using seeds

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def available_products_method
+    @product = Product.all
+    render "available_products"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.new(name: "laptop", price: 1000, image_url: "https://www.apple.com/v/macbook-pro/n/images/overview/og.png?201712041404", description: "Something you use to code.")
+product.save
+product = Product.new(name: "phone", price: 900, image_url: "https://store.storeimages.cdn-apple.com/4980/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone/x/iphone-x-silver-select-2017?wid=305&hei=358&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1515602510472", description: "Something you use to interact with people.")
+product.save
+product = Product.new(name: "headphones", price: 150, image_url: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/5577/5577872_rd.jpg;maxHeight=640;maxWidth=550", description: "Something you use to listen without other hearing.")
+product.save
