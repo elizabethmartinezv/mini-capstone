b98bddce60d2be30e7a5374b816cfad08183684d
“Elizabeth <lizzyadeline@gmail.com>
2018-06-26 19:19:34 -0500

    All products and first individual

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,16 @@
 class ProductsController < ApplicationController
   def available_products_method
-    @product = Product.all
-    render "available_products"
+    @products = Product.all
+    render "available_products.json.jbuilder"
+  end
+
+  def laptop_product_method
+    @product = Product.first
+    render "laptop_product.json.jbuilder"
+  end
+
+  def phone_product_method
+    @product = Product.first
+    render "laptop_product.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/available_products" => "products#available_products_method"
+  get "/laptop_product" => "products#laptop_product_method"
+  get "/phone_product" => "products#phone_product_method"
+  get "/headphones_product" => "products#headphones_product_method"
 end
