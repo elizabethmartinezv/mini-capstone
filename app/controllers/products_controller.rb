42654ea5d0d7e7862f7bd0f2175894a238b8507a
“Elizabeth <lizzyadeline@gmail.com>
2018-06-28 21:16:30 -0500

    Added create, update, and destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,38 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.order(:id)
     render "index.json.jbuilder"
   end
 
   def show
-    @product = Product.find_by(id: params["id"])
+    @product = Product.find_by(id: params[:id])
     render "show.json.jbuilder"
+  end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price], 
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render "show.json.jbuilder"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render json: {message: "Product successfully destroyed"}
   end
 end
