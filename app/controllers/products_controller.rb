f0510ed864a83ae6805f80fbf6fb2860672eafd5
“Elizabeth <lizzyadeline@gmail.com>
2018-06-28 19:41:30 -0500

    CHanged naming convention and used index and show actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,11 @@
 class ProductsController < ApplicationController
-  def available_products_method
+  def index
     @products = Product.all
-    render "available_products.json.jbuilder"
+    render "index.json.jbuilder"
   end
 
-  def laptop_product_method
-    @product = Product.first
-    render "product_renders.json.jbuilder"
-  end
-
-  def phone_product_method
-    @product = Product.second
-    render "product_renders.json.jbuilder"
-  end
-
-  def headphones_product_method
-    @product = Product.third
-    render "product_renders.json.jbuilder"
-  end
-
-  def params_method
+  def show
     @product = Product.find_by(id: params["id"])
-    render "product_renders.json.jbuilder"
+    render "show.json.jbuilder"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get "/available_products" => "products#available_products_method"
-  get "/laptop_product" => "products#laptop_product_method"
-  get "/phone_product" => "products#phone_product_method"
-  get "/headphones_product" => "products#headphones_product_method"
-  get "/single_product" => "products#params_method"
-  get "single_product/:id" => "products#params_method"
+  get "/products" => "products#index"
+  get "products/:id" => "products#show"
 end
