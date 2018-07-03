914a3cee6096575c2f4a83870a7aa0401da3fd74
“Elizabeth <lizzyadeline@gmail.com>
2018-07-02 21:11:38 -0500

    Add happy and sad paths

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :price, presence: true 
   validates :price, numericality: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, length: { in: 10..500 }
