1b9be64f2e95dfc83429b81ec91525184c15b2af
“Elizabeth <lizzyadeline@gmail.com>
2018-07-02 20:45:09 -0500

    Changed attributes datatypes and added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, numericality: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, length: { in: 10..500 }
+
   def discounted?
     price < 200
   end
