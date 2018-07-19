cc7d1675c583c83b3891169b3d30770e6cce4d05
“Elizabeth <lizzyadeline@gmail.com>
2018-07-18 19:42:48 -0500

    Add edit, update, and destroy actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,9 +18,30 @@ class ProductsController < ApplicationController
       name: params[:name],
       price: params[:price], 
       description: params[:description],
-      supplier_id: params[:supplier_id]
+      supplier_id: params[:supplier_id],
       )
     @product.save
     redirect_to "/products/#{@product.id}"
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.description = params[:description]
+    @product.supplier_id = params[:supplier_id]
+    @product.save
+    redirect_to "/products/#{@product.id}"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    redirect_to "/products"
   end
 end
